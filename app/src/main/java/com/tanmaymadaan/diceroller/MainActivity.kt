b9a8627a3a9b7ae90bdb51b08264ffbb929e6dbf
Tanmay Madaan <tanmaymadaan@outlook.com>
2020-06-28 18:45:43 +0530

    onClickListener

--- app/src/main/java/com/tanmaymadaan/diceroller/MainActivity.kt
+++ app/src/main/java/com/tanmaymadaan/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tanmaymadaan.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +11,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button);
         rollButton.text = "Let's Roll";
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show();
+        }
     }
 }
